a389efe5c2cccbe0f2bf7cd2714b790603fe2112
joeych8 <73697534+joeych8@users.noreply.github.com>
2021-10-13 11:07:12 +0200

    Initial Commit

---
+++ Oda/OdaApp.swift
@@ -0,0 +1,17 @@
+//
+//  OdaApp.swift
+//  Oda
+//
+//  Created by joey chau on 13/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct OdaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
